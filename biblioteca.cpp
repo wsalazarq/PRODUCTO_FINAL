f412c09204a0101be319b5217072db864d74d079
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-24 21:30:15 -0500

    Agregar biblioteca.cpp en donde se define arreglos de tipo usuario y las variables correspondientes

---
+++ biblioteca.cpp
@@ -0,0 +1,10 @@
+#include "biblioteca.h"
+#include <iostream>
+using namespace std;
+
+const int maxUsuarios = 100;
+Usuario estudiantes[maxUsuarios];
+Usuario administradores[maxUsuarios];
+
+int numEstudiantes = 0;
+int numAdministradores = 0;
