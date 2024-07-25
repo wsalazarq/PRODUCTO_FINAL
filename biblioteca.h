3ce6ae333da0e6508983d2ad2678f46005c5159b
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-24 21:27:47 -0500

    Agregar biblioteca.h en donde se define el registro usuario

---
+++ biblioteca.h
@@ -0,0 +1,13 @@
+#ifndef BIBLIOTECA_H
+#define BIBLIOTECA_H
+
+#include <iostream>
+using namespace std;
+
+struct Usuario {
+    string nombre;
+    bool esAdmin; 
+    // true si es administrador y false si es estudiante
+};
+
+#endif 
